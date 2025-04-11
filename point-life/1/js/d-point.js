62e1a3961b29c5a6e97efb05b892c2677d2bbafd
huonchan <68013384+huonchan@users.noreply.github.com>
2025-04-11 19:10:15 +0900

    dのブースターを修正してみる

--- point-life/1/js/d-point.js
+++ point-life/1/js/d-point.js
@@ -3,17 +3,18 @@ const D_LOTTERY_URLS = new Map();
 
 D_LOTTERY_URLS.set("docomo 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=ex03_me_pc_102_001_%2Fkuji%2F_99&url=https%3A%2F%2Fservice.smt.docomo.ne.jp%2Fportal%2Fspecial%2Fcollab%2Ftopics%2Fsrc%2Fdmenukuji_01.html%3Futm_source%3Ddmktportal%26utm_medium%3Downed%26utm_campaign%3Ddmktportal_daily_lottery_start&opt=kuji_continuation_visited_5%2C2025%2F04%2F02%2004%3A15%3A17%2Cbonus-visit-01%2C02");
 D_LOTTERY_URLS.set("dポイントマーケット 毎日くじ", "https://t.co/Ic7pQM64Hm");
-D_LOTTERY_URLS.set("dファッション 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/mpoJkiTW?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dジョブ 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/1cpxtacB?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("サンプル百貨店 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/vCDvTF4S?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dミュージック 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/aIeXbnYT?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("ふるさと納税百選 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/91Wn636O?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dブック 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/wMK4eyqh?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dカーシェア 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/2J51DHgH?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dヘルスケア 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/6DznlB6Z?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("dショッピング 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/dGhu7wdw?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("ドコモスポーツくじ 毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/abAvv4wA?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
-D_LOTTERY_URLS.set("Jリーグ☓毎日くじ", "https://kuji.dmarket.docomo.ne.jp/kuji/64hhlosJ?userId=717427b3ba6a12adb97dc962f53c181470d82352bbc4d88f73a3a0f035e38437&dpoint=1&month=202504&key=3a68cef0c20a818ea8815315e2a6207894ca47d742bc28896d5fd860d704cab6&device=pc");
+D_LOTTERY_URLS.set("dファッション 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_001_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DmpoJkiTW");
+D_LOTTERY_URLS.set("dジョブ 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_003_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D1cpxtacB");
+D_LOTTERY_URLS.set("サンプル百貨店 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_007_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DvCDvTF4S");
+D_LOTTERY_URLS.set("dミュージック 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_009_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DaIeXbnYT");
+D_LOTTERY_URLS.set("ふるさと納税百選 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_011_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D91Wn636O");
+D_LOTTERY_URLS.set("dブック 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_002_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DwMK4eyqh");
+D_LOTTERY_URLS.set("dカーシェア 毎日くじ","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_004_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D2J51DHgH");
+D_LOTTERY_URLS.set("dヘルスケア 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_006_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D6DznlB6Z");
+D_LOTTERY_URLS.set("dショッピング 毎日くじ","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_008_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DdGhu7wdw");
+D_LOTTERY_URLS.set("ドコモスポーツくじ 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_010_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DabAvv4wA");
+D_LOTTERY_URLS.set("Jリーグ☓毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_012_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D64hhlosJ");
+D_LOTTERY_URLS.set("すごろく(仮）", "https://dmarket.docomo.ne.jp/sugoroku/");
 
 
 function initializedD()
