a2d3e8f09adc03dae6c08ab1e09f3438860855d5
huonchan <68013384+huonchan@users.noreply.github.com>
2025-12-18 00:27:42 +0900

    sikutta

--- point-life/1/js/d-point.js
+++ point-life/1/js/d-point.js
@@ -1,12 +1,6 @@
 const D_LOTTERY_URLS = new Map();
 //無駄に時間食うやつ
 D_LOTTERY_URLS.set("ポイント広場","https://hiroba.dpoint.docomo.ne.jp/kuji/daily/");
-//クーポン
-D_LOTTERY_URLS.set("dカーシェア 毎日くじc","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_004_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D2J51DHgH");
-D_LOTTERY_URLS.set("dファッション 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_001_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DmpoJkiTW");
-D_LOTTERY_URLS.set("dブック 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_002_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DwMK4eyqh");
-D_LOTTERY_URLS.set("サンプル百貨店 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_007_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DvCDvTF4S");
-D_LOTTERY_URLS.set("dショッピング 毎日くじc","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_008_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DdGhu7wdw");
 
 //ポイント
 D_LOTTERY_URLS.set("docomo 毎日くじp", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=ex03_me_pc_102_001_%2Fkuji%2F_99&url=https%3A%2F%2Fservice.smt.docomo.ne.jp%2Fportal%2Fspecial%2Fcollab%2Ftopics%2Fsrc%2Fdmenukuji_01.html%3Futm_source%3Ddmktportal%26utm_medium%3Downed%26utm_campaign%3Ddmktportal_daily_lottery_start&opt=kuji_continuation_visited_5%2C2025%2F04%2F02%2004%3A15%3A17%2Cbonus-visit-01%2C02");
@@ -16,6 +10,12 @@ D_LOTTERY_URLS.set("dミュージック 毎日くじp", "https://dmarket.docomo.
 D_LOTTERY_URLS.set("ドコモスポーツくじ 毎日くじp", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_010_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DabAvv4wA");
 D_LOTTERY_URLS.set("dジョブ 毎日くじp", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_003_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D1cpxtacB");
 D_LOTTERY_URLS.set("Jリーグ☓毎日くじp", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_012_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D64hhlosJ");
+//クーポン
+D_LOTTERY_URLS.set("dカーシェア 毎日くじc","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_004_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D2J51DHgH");
+D_LOTTERY_URLS.set("dファッション 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_001_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DmpoJkiTW");
+D_LOTTERY_URLS.set("dブック 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_002_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DwMK4eyqh");
+D_LOTTERY_URLS.set("サンプル百貨店 毎日くじc", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_007_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DvCDvTF4S");
+D_LOTTERY_URLS.set("dショッピング 毎日くじc","https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_008_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3DdGhu7wdw");
 
 //先の時代の敗北者
 //D_LOTTERY_URLS.set("ふるさと納税百選 毎日くじ", "https://dmarket.docomo.ne.jp/portal/clicklog?lid=in01_etc_pc_101_011_/kuji/_99&url=https%3A%2F%2Fdmarket.docomo.ne.jp%2Fkuji%2Fgo_to_kuji_page.html%3Ft%3D20250411185956%26id%3D91Wn636O");
